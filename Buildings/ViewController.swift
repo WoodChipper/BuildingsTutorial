d4a9f36a86ef2f7007329405a58237347e9b3b85
WoodChipper <dgordon@Dons-MacBook.local>
2017-01-01 11:55:02 -0500

    first commit, after Alamofire hooked up; tutorial: 16:40

--- Buildings/ViewController.swift
+++ Buildings/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import Alamofire
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request("http://www.electronicarmory.com/projects/json/buildings.json")
+        
     }
 
     override func didReceiveMemoryWarning() {
